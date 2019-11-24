58603c8c506de93b044cc606faccd6397515b095
Fred Heinecke <fheinecke@ksu.edu>
2019-11-23 20:21:44 -0600

    Add project files.

---
+++ Planetbase-Deliveries/DeliveriesMod.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using PlanetbaseFramework;
+
+namespace Planetbase_Deliveries
+{
+    public class DeliveriesMod : ModBase
+    {
+        public override string ModName { get; } = "DeliveriesMod";
+
+        public const string AssemblyVersion = "1.0.0.0";
+        public override Version ModVersion { get; } = new Version(AssemblyVersion);
+
+        public override void Init()
+        {
+            InjectPatches();
+        }
+    }
+}
